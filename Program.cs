af4c5ebacf8356723be72c5ea63e9e35d113353d
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-11-26 08:47:06 -0300

    Estruturas de repetição (for, while e do while) simples.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int j = 0; j <= 10; j++)
+            Console.WriteLine(j);
+
+        int i = 0;
+        while (i < 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        Console.WriteLine("Agora o Do While");
+        i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i < 10);
+    }
+}
